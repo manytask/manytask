9822eb18d12206f0b92033b97ae87eed92ac8a52
Konstantin Chernyshev <k4black@ya.ru>
2022-08-19 14:40:16 +0200

    refactor: config file to read from_env

--- manytask/config.py
+++ manytask/config.py
@@ -21,30 +21,60 @@ class CourseConfig:
 @dataclass
 class Config:
     # tokens
-    registration_secret: str = os.environ['REGISTRATION_SECRET']
-    tester_token: str = os.environ['TESTER_TOKEN']
+    registration_secret: str
+    tester_token: str
 
     # utils
-    cache_dir: str = os.environ['CACHE_DIR']
-    solutions_dir: str = os.environ['SOLUTIONS_DIR']
+    cache_dir: str
+    solutions_dir: str
 
     # gitlab
-    gitlab_url: str = os.environ.get('GITLAB_URL', 'https://gitlab.manytask.org')
-    gitlab_admin_token: str = os.environ['GITLAB_ADMIN_TOKEN']
+    gitlab_url: str
+    gitlab_admin_token: str
     # gitlab course repos
-    gitlab_course_private_repo: str = os.environ['GITLAB_COURSE_PUBLIC_REPO']
-    gitlab_course_public_repo: str = os.environ['GITLAB_COURSE_PUBLIC_REPO']
-    gitlab_course_students_group: str = os.environ['GITLAB_COURSE_STUDENTS_GROUP']
+    gitlab_course_private_repo: str
+    gitlab_course_public_repo: str
+    gitlab_course_students_group: str
     # gitlab oauth2
-    gitlab_client_id: str = os.environ['GITLAB_CLIENT_ID']
-    gitlab_client_secret: str = os.environ['GITLAB_CLIENT_SECRET']
+    gitlab_client_id: str
+    gitlab_client_secret: str
 
-    # google sheets (credentials base64 encoded json)
-    gdoc_url: str = os.environ.get('GDOC_URL', 'https://docs.google.com')
-    gdoc_account_credentials_base64: str = os.environ['GDOC_ACCOUNT_CREDENTIALS_BASE64']
+    # google sheets
+    gdoc_url: str
+    gdoc_account_credentials_base64: str  # base64 encoded json
     # google public sheet
-    gdoc_spreadsheet_id: str = os.environ['GDOC_SPREADSHEET_ID']
-    gdoc_scoreboard_sheet: int = int(os.environ.get('GDOC_SCOREBOARD_SHEET', 0))
+    gdoc_spreadsheet_id: str
+    gdoc_scoreboard_sheet: int
+
+    @classmethod
+    def from_env(cls) -> Config:
+        return cls(
+            # tokens
+            registration_secret=os.environ['REGISTRATION_SECRET'],
+            tester_token=os.environ['TESTER_TOKEN'],
+
+            # utils
+            cache_dir=os.environ['CACHE_DIR'],
+            solutions_dir=os.environ['SOLUTIONS_DIR'],
+
+            # gitlab
+            gitlab_url=os.environ.get('GITLAB_URL', 'https://gitlab.manytask.org'),
+            gitlab_admin_token=os.environ['GITLAB_ADMIN_TOKEN'],
+            # gitlab course repos
+            gitlab_course_private_repo=os.environ['GITLAB_COURSE_PUBLIC_REPO'],
+            gitlab_course_public_repo=os.environ['GITLAB_COURSE_PUBLIC_REPO'],
+            gitlab_course_students_group=os.environ['GITLAB_COURSE_STUDENTS_GROUP'],
+            # gitlab oauth2
+            gitlab_client_id=os.environ['GITLAB_CLIENT_ID'],
+            gitlab_client_secret=os.environ['GITLAB_CLIENT_SECRET'],
+
+            # google sheets (credentials base64 encoded json)
+            gdoc_url=os.environ.get('GDOC_URL', 'https://docs.google.com'),
+            gdoc_account_credentials_base64=os.environ['GDOC_ACCOUNT_CREDENTIALS_BASE64'],
+            # google public sheet
+            gdoc_spreadsheet_id=os.environ['GDOC_SPREADSHEET_ID'],
+            gdoc_scoreboard_sheet=int(os.environ.get('GDOC_SCOREBOARD_SHEET', 0)),
+        )
 
 
 @dataclass
@@ -54,8 +84,8 @@ class DebugConfig(Config):
     tester_token: str = 'tester_token'
 
     # utils
-    cache_dir: str = './cache'
-    solutions_dir: str = './solutions'
+    cache_dir: str = './tmp/cache'
+    solutions_dir: str = './tmp/solutions'
 
     # gitlab
     gitlab_url: str = 'https://gitlab.manytask.org'
@@ -69,13 +99,27 @@ class DebugConfig(Config):
     gitlab_client_secret: str = ''
 
     # google sheets
-    gdoc_url: str = 'https://docs.google.com'
-    gdoc_account: str = os.environ['GDOC_ACCOUNT']
+    gdoc_url: str = ''
+    gdoc_account_credentials_base64: str = ''
     # google public sheet
-    gdoc_spreadsheet_id: str = os.environ.get('GDOC_SPREADSHEET_ID', '1cRah9NC5Nl7_NyzttC3Q5BtrnbdO6KyaG7gx5ZGusTM')
-    gdoc_scoreboard_sheet: int = int(os.environ.get('GDOC_SCOREBOARD_SHEET', 0))
+    gdoc_spreadsheet_id: str = ''
+    gdoc_scoreboard_sheet: int = ''
+
+    @classmethod
+    def from_env(cls) -> Config:
+        return cls(
+            gdoc_url='https://docs.google.com',
+            gdoc_account_credentials_base64=os.environ['GDOC_ACCOUNT_CREDENTIALS_BASE64'],
+            gdoc_spreadsheet_id=os.environ.get('GDOC_SPREADSHEET_ID', '1cRah9NC5Nl7_NyzttC3Q5BtrnbdO6KyaG7gx5ZGusTM'),
+            gdoc_scoreboard_sheet=int(os.environ.get('GDOC_SCOREBOARD_SHEET', 0)),
+        )
 
 
 @dataclass
 class TestConfig(DebugConfig):
-    gdoc_account: str = ''
+    # google sheets
+    gdoc_url: str = ''
+    gdoc_account_credentials_base64: str = ''
+    # google public sheet
+    gdoc_spreadsheet_id: str = ''
+    gdoc_scoreboard_sheet: int = 0
