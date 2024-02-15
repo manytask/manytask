d6368845a68e0ec39d14879566043b724f8da042
Konstantin Chernyshev <k4black@ya.ru>
2024-02-15 16:34:08 +0100

    fix: remove group.shared_runners_enabled

--- manytask/glab.py
+++ manytask/glab.py
@@ -202,7 +202,7 @@ class GitLabApi:
                 "forking_access_level": "disabled",
                 # Enable shared runners
                 # TODO: Relay on groups runners
-                "shared_runners_enabled": course_public_project.shared_runners_enabled,
+                "shared_runners_enabled": course_group.shared_runners_setting.get("enabled") or False,
                 # Set external gitlab-ci config from public repo
                 "ci_config_path": f".gitlab-ci.yml@{course_public_project.path_with_namespace}",
                 # Merge method to squash
