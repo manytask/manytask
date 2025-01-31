f2f777d37021db423cec003568a144518c1d3e24
seliverstow <seliverstow@yandex-team.ru>
2025-01-31 10:40:27 +0300

    fix: lint

--- manytask/database_utils.py
+++ manytask/database_utils.py
@@ -11,22 +11,15 @@ def get_database_table_data() -> dict[str, Any]:
 
     storage_api = course.storage_api
     all_scores = storage_api.get_all_scores()
-    
+
     all_tasks = []
     if course.deadlines:
         for group in course.deadlines.get_groups():
             for task in group.tasks:
                 if task.enabled:
-                    all_tasks.append({
-                        'name': task.name,
-                        'score': 0,
-                        'group': group.name
-                    })
+                    all_tasks.append({"name": task.name, "score": 0, "group": group.name})
 
-    table_data = {
-        'tasks': all_tasks,
-        'students': []
-    }
+    table_data = {"tasks": all_tasks, "students": []}
 
     for username, student_scores in all_scores.items():
         total_score = sum(student_scores.values())
