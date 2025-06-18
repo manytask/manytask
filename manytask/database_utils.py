5bffaf17ea189010153775fc24bbd554f517e5e1
dmasloff <74042473+dmasloff@users.noreply.github.com>
2025-06-18 11:43:35 +0300

    feat: allow to set large task type in configs (#424)

--- manytask/database_utils.py
+++ manytask/database_utils.py
@@ -10,6 +10,7 @@ def get_database_table_data(app: CustomFlask, course_name: str) -> dict[str, Any
     all_scores = storage_api.get_all_scores(course_name)
 
     all_tasks = []
+    large_tasks = []
     max_score: int = 0
     for group in storage_api.get_groups(course_name):
         for task in group.tasks:
@@ -17,11 +18,14 @@ def get_database_table_data(app: CustomFlask, course_name: str) -> dict[str, Any
                 all_tasks.append({"name": task.name, "score": 0, "group": group.name})
                 if not task.is_bonus:
                     max_score += task.score
+                if task.is_large:
+                    large_tasks.append(task.name)
 
     table_data = {"tasks": all_tasks, "students": []}
 
     for username, student_scores in all_scores.items():
         total_score = sum(student_scores.values())
+        large_count = sum(1 for task in large_tasks if student_scores.get(task, 0) > 0)
         student_name = app.gitlab_api.get_student_by_username(username).name
         table_data["students"].append(
             {
@@ -30,6 +34,7 @@ def get_database_table_data(app: CustomFlask, course_name: str) -> dict[str, Any
                 "scores": student_scores,
                 "total_score": total_score,
                 "percent": 0 if max_score == 0 else total_score * 100.0 / max_score,
+                "large_count": large_count,
             }
         )
 
