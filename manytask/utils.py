6938f2fcb29f7f5576c85a9af31f67fd227f1de6
cladenkiy <d.melentsov123@gmail.com>
2025-06-28 02:21:20 +0300

    refactor: linter fix #2

--- manytask/utils.py
+++ manytask/utils.py
@@ -8,6 +8,7 @@ from manytask.main import CustomFlask
 def generate_token_hex(bytes_count: int = 24) -> str:
     return secrets.token_hex(nbytes=bytes_count)
 
+
 def get_courses(app: CustomFlask):
     if app.debug:
         courses_names = app.storage_api.get_all_courses_names()
