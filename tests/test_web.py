09e5c2f9e44e3d46a7cb2763f1eb23ff2c1add31
Gorobets Ivan <96993672+KIoppert@users.noreply.github.com>
2025-07-28 12:36:30 +0300

    feat: remove gitlab admins + add admin panel + ability to change the name (#474)
    Now, the admin status stored in DB, not in GitLab
    We have 2 admin statuses:
    
    Instance admin - it can edit/create courses, change user info, add new admins and have all permissions of a course admin.
    Course admin - it can change marks in course table.
    
    * feat: add instance admin role management system
    
    * refactor: clean up code formatting and remove unused imports
    
    * feat: add admin panel link and update user profile method to handle optional parameters

--- tests/test_web.py
+++ tests/test_web.py
@@ -9,6 +9,7 @@ from zoneinfo import ZoneInfo
 import pytest
 from authlib.integrations.base_client import OAuthError
 from flask import Flask, url_for
+from flask_wtf import CSRFProtect
 
 from manytask.abstract import StoredUser
 from manytask.api import bp as api_bp
@@ -57,7 +58,6 @@ def app(mock_gitlab_api, mock_storage_api):
 def mock_gitlab_api():
     class MockGitlabApi:
         def __init__(self):
-            self.course_admin = False
             self.base_url = GITLAB_BASE_URL
 
         @staticmethod
@@ -96,9 +96,6 @@ def mock_gitlab_api():
         def _parse_user_to_student(user: dict[str, Any]):
             return Student(id=TEST_USER_ID, username=TEST_USERNAME, name=TEST_STUDENT_NAME)
 
-        def check_is_course_admin(self, _user_id, _course_group):
-            return self.course_admin
-
     return MockGitlabApi()
 
 
@@ -110,6 +107,7 @@ def mock_storage_api(mock_course):  # noqa: C901
                 username=TEST_USERNAME, first_name=TEST_FIRST_NAME, last_name=TEST_LAST_NAME, course_admin=False
             )
             self.course_name = TEST_COURSE_NAME
+            self.instance_admin = False
 
         @staticmethod
         def get_scores_update_timestamp(_course_name):
@@ -147,7 +145,7 @@ def mock_storage_api(mock_course):  # noqa: C901
             course_admin: bool,
         ) -> StoredUser:
             self.stored_user.username = username
-            self.stored_user.course_admin = self.stored_user.course_admin or course_admin
+            self.stored_user.course_admin = self.stored_user.course_admin or self.instance_admin
             return self.stored_user
 
         @staticmethod
@@ -172,6 +170,9 @@ def mock_storage_api(mock_course):  # noqa: C901
 
         def get_stored_user(self, _course_name, _username):
             return self.stored_user
+
+        def check_if_instance_admin(self, _username):
+            return self.instance_admin
 
         def check_if_course_admin(self, _course_name, _username):
             return self.stored_user.course_admin
@@ -356,6 +357,7 @@ def check_admin_status_code(response, check_true):
 @pytest.mark.parametrize("get_param_admin", ["true", "1", "yes", None, "false", "0", "no", "random_value"])
 def test_course_page_user_sync(app, mock_gitlab_oauth, mock_course, path_and_func, debug, get_param_admin):
     path, check_func = path_and_func
+    CSRFProtect(app)
 
     if get_param_admin is not None:
         path += f"?admin={get_param_admin}"
@@ -373,7 +375,7 @@ def test_course_page_user_sync(app, mock_gitlab_oauth, mock_course, path_and_fun
             app.oauth = mock_gitlab_oauth
             app.debug = debug
 
-            # not admin in gitlab, not admin in manytask
+            # not instance admin, not course admin
             response = client.get(path)
 
             if app.debug:
@@ -389,29 +391,10 @@ def test_course_page_user_sync(app, mock_gitlab_oauth, mock_course, path_and_fun
                     "user_id": TEST_USER_ID,
                     "access_token": TEST_TOKEN,
                 }
-            app.gitlab_api.course_admin = True
-
-            # admin in gitlab, not admin in manytask
-            response = client.get(path)
-
-            if app.debug:
-                # in debug admin flag is the same as get param
-                check_func(response, get_param_admin in ("true", "1", "yes", None))
-            else:
-                check_func(response, True)
-
-            with client.session_transaction() as sess:
-                sess["gitlab"] = {
-                    "version": TEST_VERSION,
-                    "username": TEST_USERNAME,
-                    "user_id": TEST_USER_ID,
-                    "access_token": TEST_TOKEN,
-                }
-            app.gitlab_api.course_admin = False
 
             app.storage_api.stored_user.course_admin = True
 
-            # not admin in gitlab, admin in manytask
+            # not instance admin, but course admin
             response = client.get(path)
 
             if app.debug:
@@ -427,8 +410,10 @@ def test_course_page_user_sync(app, mock_gitlab_oauth, mock_course, path_and_fun
                     "user_id": TEST_USER_ID,
                     "access_token": TEST_TOKEN,
                 }
+            app.storage_api.stored_user.course_admin = False
+            app.storage_api.instance_admin = True
 
-            # admin in gitlab, admin in manytask
+            # instance admin => course admin
             response = client.get(path)
 
             if app.debug:
