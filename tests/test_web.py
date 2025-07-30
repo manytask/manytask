4822298a1e0c44d8e34f1fb8eb8d4355589973f7
dmasloff <dmaslov24@gmail.com>
2025-07-30 16:54:20 +0300

    feat: web test fix

--- tests/test_web.py
+++ tests/test_web.py
@@ -104,6 +104,10 @@ def mock_gitlab_api():
 
 @pytest.fixture
 def mock_storage_api(mock_course):  # noqa: C901
+    class MockFinalGradeConfig:
+        def evaluate(self, *_args, **_kwargs):
+            return 5
+
     class MockStorageApi:
         def __init__(self):
             self.stored_user = StoredUser(
@@ -157,7 +161,7 @@ def mock_storage_api(mock_course):  # noqa: C901
 
         @staticmethod
         def get_grades(*_args, **_kwargs):
-            return {}
+            return MockFinalGradeConfig()
 
         @staticmethod
         def get_course(_name):
