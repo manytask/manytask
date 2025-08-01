6273308b0df96f6fcf01f2772adf9e4946454eee
Artem Zhmurov <zhmurov@gmail.com>
2025-08-01 19:06:24 +0300

    Fix ruff

--- manytask/glab.py
+++ manytask/glab.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 import logging
 from dataclasses import dataclass
 from typing import Any
-from requests.exceptions import HTTPError
 
 import gitlab
 import gitlab.const
 import gitlab.v4.objects
 import requests
+from requests.exceptions import HTTPError
 
 from .abstract import RmsApi, RmsUser
 
